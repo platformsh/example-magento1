7a802b1e48a8b626e361b704f1df0c4b5236a82d
John Grubb <JGrubb@users.noreply.github.com>
2017-01-21 17:28:13 -0500

    Comment on $debugMode

--- Platformsh.php
+++ Platformsh.php
@@ -9,7 +9,10 @@ class Platformsh
 
     // This is the Magento root
     protected $webRoot = 'web';
-
+    // $debugMode provides a wealth of helpful logging that prints to std out
+    // during the build phase and the deploy log during deploy.  None of this
+    // is accessible to the outside world, but of course feel free to 
+    // set this for FALSE once you get your project set up.
     protected $debugMode = TRUE;
 
     protected $platformReadWriteDirs = ['var', 'app/etc', 'media'];
